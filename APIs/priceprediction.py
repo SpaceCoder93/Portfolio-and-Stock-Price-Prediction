18c7f91bedddbd20225e6bcc0b0a958f99c5a616
Viraj Paradkar <virajsparadkar@gmail.com>
2023-11-06 10:24:41 +0530

    Code Changes

--- APIs/priceprediction.py
+++ APIs/priceprediction.py
@@ -12,16 +12,17 @@ from bs4 import BeautifulSoup
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 class Predict:
-    def __init__(ticker_ID, parameter):
+    def __init__(self, ticker_id, parameter):
+        global date, time
         news_tables = {}
         finviz_url = 'https://finviz.com/quote.ashx?t='
-        url = finviz_url + ticker_ID
+        url = finviz_url + ticker_id
         req = Request(url=url, headers={'user-agent': 'my-app'})
         response = urlopen(req)
         html = BeautifulSoup(response, 'html')
         news_table = html.find(id='news-table')
-        news_tables[ticker_ID] = news_table
-        web_data = news_tables[ticker_ID]
+        news_tables[ticker_id] = news_table
+        web_data = news_tables[ticker_id]
         web_data_rows = web_data.findAll('tr')
         parse = []
         for ticker, news_table in news_tables.items():
@@ -44,9 +45,9 @@ class Predict:
         year = now.strftime("%Y")
         month = now.strftime("%m")
         day = now.strftime("%d")
-        end_date = (f"{year}, {month}, {day}")
+        end_date = f"{year}, {month}, {day}"
         past_days = int(30)
-        data = web.DataReader(ticker_ID, "yahoo", start_date, end_date)
+        data = web.DataReader(ticker_id, "yahoo", start_date, end_date)
         scalar = MinMaxScaler(feature_range=(0,1))
         scaled_data = scalar.fit_transform(data[parameter].values.reshape(-1,1))
         x_train = []
@@ -66,7 +67,7 @@ class Predict:
         training_model.add(Dense(units=1))
         training_model.compile(optimizer = 'adam', loss = 'mean_squared_error')
         training_model.fit(x_train, y_train, epochs=30, batch_size=32)
-        test_data = web.DataReader(ticker_ID, 'yahoo', start_date, end_date)
+        test_data = web.DataReader(ticker_id, 'yahoo', start_date, end_date)
         actual_prices = test_data['Close'].values
         total_dataset = pd.concat((data['Close'], test_data[parameter]), axis=0)
         model_input = total_dataset[len(total_dataset) - len(test_data) - past_days:].values
@@ -77,20 +78,19 @@ class Predict:
                 x_test.append(model_input[x-past_days:x, 0])
         x_test = np.array(x_test)
         x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-        Predicted_prices = training_model.predict(x_test)
-        Predicted_prices = scalar.inverse_transform(Predicted_prices)
-        plt.plot(actual_prices, color='black', label=f'Actual {ticker_ID} Price')
-        plt.plot(Predicted_prices, color='green', label=f'Predicted {ticker_ID} Price')
-        plt.title(f'{ticker_ID} Share Price')
+        predicted_prices = training_model.predict(x_test)
+        predicted_prices = scalar.inverse_transform(predicted_prices)
+        plt.plot(actual_prices, color='black', label=f'Actual {ticker_id} Price')
+        plt.plot(predicted_prices, color='green', label=f'Predicted {ticker_id} Price')
+        plt.title(f'{ticker_id} Share Price')
         plt.xlabel('Time')
-        plt.ylabel(f'{ticker_ID} Share Price')
+        plt.ylabel(f'{ticker_id} Share Price')
         plt.legend()
         plt.show()
-        quote = web.DataReader(ticker_ID, 'yahoo', start_date, end_date)
+        quote = web.DataReader(ticker_id, 'yahoo', start_date, end_date)
         df_scalar = MinMaxScaler(feature_range=(0,1))
         df_scaled_data = df_scalar.fit_transform(data[parameter].values.reshape(-1,1))
-        df_x_test = []
-        df_x_test.append(df_scaled_data)
+        df_x_test = [df_scaled_data]
         df_x_test = np.array(x_test)
         df_x_test = np.reshape(df_x_test, (df_x_test.shape[0], df_x_test.shape[1], 1))
         predict = training_model.predict(df_x_test)
@@ -110,3 +110,5 @@ class Predict:
         final_price_to_display = a + c
         final_round = round(final_price_to_display, 2)
         print(final_round)
+        
+Predict("TSLA", "Open")
--- main.py
+++ main.py
@@ -5,8 +5,24 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('StockGuru')
-        self.attributes('-fullscreen', True)
+        self.state('zoomed')
         self.mainloop()
+
+class LoginPage(tk.Frame):
+    def __init__(self):
+        pass
+class CreatePage(tk.Frame):
+    def __init__(self):
+        pass
+class Verification(tk.Frame):
+    def __init__(self):
+        pass
+class DashBoard(tk.Frame):
+    def __init__(self):
+        pass
+class Prediction(tk.Frame):
+    def __init__(self):
+        pass
 
 if __name__ == "__main__":
     App()
